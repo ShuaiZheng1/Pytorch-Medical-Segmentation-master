94fc7200b599d157e9e46061564e9ed69a5f130e
ShuaiZheng1 <86041968+ShuaiZheng1@users.noreply.github.com>
2021-06-22 19:40:46 +0800

    Add files via upload

---
+++ Pytorch-Medical-Segmentation-master - 副本/hparam.py
@@ -0,0 +1,23 @@
+class hparams:
+
+    train_or_test = 'train'
+    debug = False
+    mode = '3d' # '2d or '3d'
+    in_class = 1
+    out_class = 1
+
+    crop_or_pad_size = 256
+    patch_size = 128
+
+    fold_arch = '*.mhd'
+
+    source_train_dir = 'source_dataset'
+    label_train_dir = 'label_dataset'
+    source_test_dir = 'img'
+    label_test_dir = 'label'
+    #source_train_dir = 'img'
+    #label_train_dir = 'label'
+    #source_test_dir = 'img'
+    #label_test_dir = 'label'
+
+    output_dir_test = 'results'
